cb41f7ed8ee14026f444edc5ec683f2572f11090
user <jerby.pardo28@gmail.com>
2019-12-03 23:36:18 +0800

    Added classes to the application

---
+++ src/classgradingsystem/Student.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classgradingsystem;
+
+import java.util.*;
+
+/**
+ *
+ * @author user
+ */
+public abstract class Student extends User{
+    private int student_id;
+    private float general_average;
+    private List<ClassRecord> classRecordList;
+    
+    public List<ClassRecord> getClassRecordList() {
+        return classRecordList;
+    }
+
+    public void setClassRecordList(List<ClassRecord> classRecordList) {
+        this.classRecordList = classRecordList;
+    }
+    
+    public int getStudentId() {
+        return student_id;
+    }
+
+    public void setStudentId(int student_id) {
+        this.student_id = student_id;
+    }
+
+    public float getGeneralAverage() {
+        return general_average;
+    }
+
+    public void setGeneralAverage(float general_average) {
+        this.general_average = general_average;
+    }
+    
+//    public ClassRecord getClassRecord(int class_id){
+//        
+//    } 
+    
+//    public boolean addClassRecord(ClassRecord classRecord){
+//        
+//    }
+//    
+//    public boolean removeClassRecord(int class_id){
+//        
+//    }
+//    
+//    public boolean updateClassRecord(ClassRecord classRecord){
+//        
+//    }
+//    
+    public Student(int user_id, String username, String password, String firstname, String lastname) {
+        super(user_id, username, password, firstname, lastname);
+    }
+    
+    @Override
+    public void displayName (String firstname, String lastname){
+        System.out.println("First Name: "+firstname);
+        System.out.println("Last Name: "+lastname);
+    }
+     
+//    @Override
+//    public String toString(){
+////        return "Username:" + username + "First Name:";
+//    } 
+
+}
