348084a6b95084d19512c0d6ba08a3d2b76536f7
user <jerby.pardo28@gmail.com>
2019-12-03 23:37:18 +0800

    Added classes to the application

---
+++ src/classgradingsystem/Subject.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classgradingsystem;
+
+/**
+ *
+ * @author user
+ */
+public class Subject {
+    private int subject_id;
+    private String subject_name;
+
+    public int getSubject_id() {
+        return subject_id;
+    }
+
+    public void setSubject_id(int subject_id) {
+        this.subject_id = subject_id;
+    }
+
+    public String getSubject_name() {
+        return subject_name;
+    }
+
+    public void setSubject_name(String subject_name) {
+        this.subject_name = subject_name;
+    }
+    
+//    public String toString(){
+//        
+//    }
+}
